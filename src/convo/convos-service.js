39cbf14d66f2c9b11295aa6d6fc357d9c4d43986
Noah Turner <aminnoah@yahoo.com>
2020-12-28 19:40:44 -0600

    heroku set up

--- src/convo/convos-service.js
+++ src/convo/convos-service.js
@@ -24,15 +24,104 @@ const ConvosService = {
 //         })
 // },
 getConvosByUserIdAndEventList(db, user_id, event_list) {
-    let eventListFieldName = `convos.ok_for_${event_list}`
-    console.log(eventListFieldName)
-    return db
-    .select('*')
+    if(event_list == 'fashion') {
+      return db
+            .select('*')
             .from('convos')
             .where({
-                'user_id': user_id,
+              'is_public': true,
                 'ok_for_fashion': true
             })
+    }
+    if(event_list == 'work') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_work': true
+            })
+    }
+    if(event_list == 'entertainment') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_entertainment': true
+            })
+    }
+    if(event_list == 'exercise') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_exercise': true
+            })
+    }
+    if(event_list == 'travel') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_travel': true
+            })
+    }
+    if(event_list == 'technology') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_technology': true
+            })
+    }
+    if(event_list == 'holidays') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_holidays': true
+            })
+    }
+    if(event_list == 'education') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_education': true
+            })
+    }
+    if(event_list == 'food') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_food': true
+            })
+    }
+    if(event_list == 'leisure') {
+      return db
+            .select('*')
+            .from('convos')
+            .where({
+                'is_public': true,
+                'ok_for_leisure': true
+            })
+    }
+    if(is_public == false) {
+      return db
+        .select('*')
+        .from('convos')
+        .where({
+          'is_public': false
+        })
+    }
     },
   //relevant
   insertConvos(db, newConvos) {
